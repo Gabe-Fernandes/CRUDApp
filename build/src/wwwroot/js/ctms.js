c67f2db246b6f0a898f8bb84ff3864a318fc924c
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2022-05-28 16:47:13 -0400

    v1 ctms backend: crud operations, responsive design, client/server validation, transitions under dev

--- build/src/wwwroot/js/ctms.js
+++ build/src/wwwroot/js/ctms.js
@@ -1,8 +1,8 @@
 const adminViewUsersBtn = document.getElementById("adminViewUsersBtn");
-const adminCreateStudyBtn = document.getElementById("adminCreateStudyBtn");
+//const adminCreateStudyBtn = document.getElementById("adminCreateStudyBtn");
 
-const userModal = document.getElementById("userModal");
-const userBackBtn = document.getElementById("userBackBtn");
+//const userModal = document.getElementById("userModal");
+//const userBackBtn = document.getElementById("userBackBtn");
 
 const createModal = document.getElementById("createModal");
 const createStudyName = document.getElementById("createStudyName");
@@ -10,38 +10,30 @@ const createDescription = document.getElementById("createDescription");
 const createRegion = document.getElementById("createRegion");
 const createStartDate = document.getElementById("createStartDate");
 const createEndDate = document.getElementById("createEndDate");
-const createCreateBtn = document.getElementById("createCreateBtn");
-const createBackBtn = document.getElementById("createBackBtn");
+const endDateError = document.getElementById("endDateError");
+//const createCreateBtn = document.getElementById("createCreateBtn");
+//const createBackBtn = document.getElementById("createBackBtn");
 const createError = document.getElementById("createError");
 
-function CreateErrorToggle(toggle, message) {
+function CreateErrorToggle(toggle, opposite) {
   createError.classList[toggle]("create-error");
-  createCreateBtn.innerHTML = message;
   createCreateBtn.classList[toggle]("btn-disable");
+  endDateError.classList[opposite]("create-error-txt-hide");
 }
 
-function VerifyStudy() {
-  if (createStartDate.value > createEndDate.value && createEndDate.value !== "") {
-    CreateErrorToggle("add", "Invalid Dates");
-  }else {
-    RemoveModal(createModal);
-    Sleep(1000).then(()=> {ClearCreateModal()});
+createModal.addEventListener("submit", function (evt) {
+  if(createStartDate.value > createEndDate.value && createEndDate.value !== "") {
+    CreateErrorToggle("add", "remove");
+    evt.preventDefault();
   }
-}
+});
 
-function ClearCreateModal() {
-  createStudyName.value = "";
-  createDescription.value = "";
-  createRegion.value = "";
-  createStartDate.value = "";
-  createEndDate.value = "";
-}
 
-adminViewUsersBtn.addEventListener("click", ()=> {AddModal(userModal)});
-userBackBtn.addEventListener("click", ()=> {RemoveModal(userModal)});
-adminCreateStudyBtn.addEventListener("click", ()=> {AddModal(createModal)});
-createBackBtn.addEventListener("click", ()=> {RemoveModal(createModal)});
+//adminViewUsersBtn.addEventListener("click", ()=> {AddModal(userModal)});
+//userBackBtn.addEventListener("click", ()=> {RemoveModal(userModal)});
+//adminCreateStudyBtn.addEventListener("click", ()=> {AddModal(createModal)});
+//createBackBtn.addEventListener("click", ()=> {RemoveModal(createModal)});
 
-createCreateBtn.addEventListener("click", ()=> {VerifyStudy()});
-createStartDate.addEventListener("input", ()=> {CreateErrorToggle("remove", "Create")});
-createEndDate.addEventListener("input", ()=> {CreateErrorToggle("remove", "Create")});
+//createCreateBtn.addEventListener("click", ()=> {ValidateStudy()});
+createStartDate.addEventListener("input", ()=> {CreateErrorToggle("remove", "add")});
+createEndDate.addEventListener("input", () => { CreateErrorToggle("remove", "add")});
